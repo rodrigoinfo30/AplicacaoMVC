a8f39011f17191d841abaa81371dcf8b708efaa0
Rodrigo Santos <80109080+rodrigoinfo30@users.noreply.github.com>
2026-01-16 13:10:21 -0300

    Add files via upload

---
+++ ReportRotativa/ReportRotativa/App_Start/BundleConfig.cs
@@ -0,0 +1,63 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MoneyReport
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Layouts/Menu/css").Include(
+          "~/Layouts/Menu/css/bootstrap.css",
+          "~/Layouts/Menu/css/bootstrap.min.css",
+          "~/Layouts/Menu/css/Buttonbusca.css",
+          "~/Layouts/Menu/css/component.css",
+          "~/Layouts/Menu/css/componentrel.css",
+          "~/Layouts/Menu/css/demo.css",
+          "~/Layouts/Menu/css/demo2.css",
+          "~/Layouts/Menu/css/demorel.css",
+          "~/Layouts/Menu/css/modal.css",
+          "~/Layouts/Menu/css/normalize.css"));
+
+            //#if DEBUG
+            //            BundleTable.EnableOptimizations = false;
+            //#else
+            //            BundleTable.EnableOptimizations = true;
+            //#endif
+
+            //            bundles.Add(new ScriptBundle("~/bundles/jquerymobile").Include("~/Scripts/jquery.mobile-{version}.js"))
+
+            //  bundles.Add(new StyleBundle("~/Menu/js").Include(
+            //"~/Menu/css/bootstrap.css",
+            //"~/Menu/css/bootstrap.min.css",
+            //"~/Menu/css/Buttonbusca.css",
+            //"~/Menu/css/component.css",
+            //"~/Menu/css/componentrel.css",
+            //"~/Menu/css/demo.css",
+            //"~/Menu/css/demo2.css",
+            //"~/Menu/css/demorel.css",
+            //"~/Menu/css/modal.css",
+            //"~/Menu/css/normalize.css"));
+        }
+    }
+}
---
+++ ReportRotativa/ReportRotativa/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MoneyReport
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Cedentes",
+                "Relatorios/Cedentes",
+                new { controller = "Relatorios", action = "Cedentes", id = 0 }
+            );
+
+            routes.MapRoute(
+                "RelAberto",
+                "Relatorios/RelAberto",
+                new { controller = "Relatorios", action = "RelAberto", id = 0 }
+            );
+
+            routes.MapRoute(
+                "ExecAberto",
+                "Relatorios/ExecAberto",
+                new { controller = "Relatorios", action = "ExecAberto", id = 0 }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
